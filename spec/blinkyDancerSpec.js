e1ea6442f45e73f93542dd5047e51982d1659db7
Hack Reactor Student <student@hackreactor.com>
2017-08-28 21:47:59 -0700

    Implement SpinnyDancer and unit tests for all dancers

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -32,3 +32,80 @@ describe('blinkyDancer', function() {
     });
   });
 });
+
+describe('bouncyDancer', function() {
+
+  var bouncyDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    bouncyDancer = new BouncyDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(bouncyDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a step function that moves the x and y positions', function() {
+    //sinon.spy(bouncyDancer.$node, 'toggle');
+    var x = bouncyDancer.x;
+    var y = bouncyDancer.y;
+    bouncyDancer.step();
+    expect(bouncyDancer.x).to.not.equal(x);
+    expect(bouncyDancer.y).to.not.equal(y);
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(bouncyDancer, 'step');
+      expect(bouncyDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(bouncyDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(bouncyDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
+
+
+describe('spinnyDancer', function() {
+
+  var spinnyDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    spinnyDancer = new SpinnyDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(spinnyDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a step function that moves the x and y positions', function() {
+    //sinon.spy(bouncyDancer.$node, 'toggle');
+    var x = spinnyDancer.x;
+    var y = spinnyDancer.y;
+    spinnyDancer.step();
+    expect(spinnyDancer.x).to.not.equal(x);
+    expect(spinnyDancer.y).to.not.equal(y);
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(spinnyDancer, 'step');
+      expect(spinnyDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(spinnyDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(spinnyDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
--- src/spinnyDancer.js
+++ src/spinnyDancer.js
@@ -1,7 +1,13 @@
 var SpinnyDancer = function(top, left, timeBetweenSteps) {
   this.oldStep = Dancer.prototype.step;
+  this.centerX = left;
+  this.centerY = top;
+  this.x = 0;
+  this.y = 0;
+  this.r = 25;
+  this.theta = 0;
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node.addClass('blinky');
+  this.$node.addClass('spinny');
 };
 
 SpinnyDancer.prototype = Object.create(Dancer.prototype);
@@ -9,6 +15,11 @@ SpinnyDancer.prototype.constructor = SpinnyDancer;
 
 SpinnyDancer.prototype.step = function() {
   this.oldStep();
-  this.$node.toggle();
+  
+  this.x = this.centerX + this.r * Math.cos(this.theta);
+  this.y = this.centerY + this.r * Math.sin(this.theta);
+  this.theta += Math.PI / 8;
+  
+  this.setPosition(this.y, this.x);
 };
 
